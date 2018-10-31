eb2779873d6f1440875e805290fbb167caa09846
Michael Soares <mikey.soares@gmail.com>
2018-10-31 17:29:22 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+def initialize(dogs_name)
+  @dogs_name = dogs_name
+  end
+  def dogs_name=(dogs_name)
+    @dogs_name = dogs_name
+  end
+  def dogs_name
+    @dogs_name
+  end
+end
+
+  
