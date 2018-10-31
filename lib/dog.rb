ded830af53a61de44b024279745aa492aa7c4051
Michael Soares <mikey.soares@gmail.com>
2018-10-31 17:32:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-def initialize(name)
+def initialize(name, breed)
   @name = name
+  @breed = breed
   end
   def name=(name)
     @name = name
@@ -8,4 +9,12 @@ def initialize(name)
   def name
     @name
   end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
 end
+
+fido = Dog.new("")
